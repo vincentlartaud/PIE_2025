6e7a046a019622b2e2ca01f0c58e11244fdb7eab
vlartaud <lartaudvincent@gmail.com>
2025-04-18 14:28:00 +0200

    fichier header de fonctions de déplacement

---
+++ Arduino/depl_sous_marin1/depl_sous_marin1.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+Servo moteur1;
+Servo moteur2;
+
+const int pinMoteur1 = 9;
+const int pinMoteur2 = 10;
+
+// Définition des vitesses
+const int throttleStop = 2000; // Moteur arrêté
+const int throttleNeutre = 1500; // Point mort (neutre) 
+const int throttleAvant = 1300; // Marche avant
+const int throttleArriere = 1700; // Marche arrière
+const int throttleVirage = 200 ; // Virage (vitesse relative par rapport au neutre)
+
+void setup(){
+  Serial.begin(9600);
+
+  moteur1.attach(pinMoteur1);
+  moteur2.attach(pinMoteur2);
+
+  // Calibrage 
+  calibrerESCs();
+
+  // Mise au neutre après calibrage
+  avance(throttleNeutre,120000) // Pause de 2min le temps de refermer le bolide et de le mettre à l'eau
+}
+
+void loop(){
+  for (int i=1; i<=10; i++) {
+    Serial.print("=== Cycle ");
+    Serial.print(i);
+    Serial.println(" ===");
+
+    avance(throttleAvant,3000);
+    delay(2000);
+    tourne_gauche(throttleVirage,3000);
+    delay(2000);
+    avance(throttleAvant,3000);
+    delay(2000);
+    tourne_droite(throttleVirage,3000);
+    delay(2000);
+    avance(5000);
+  }
+
+  // Fin de la boucle, arrêt complet
+  Serial.println("Séquence terminée ! Arrêt des moteurs.");
+  arret();
+  while(true);  // Boucle infinie pour ne pas recommencer
+}
---
+++ Arduino/depl_sous_marin1/fonctions.h
@@ -0,0 +1,50 @@
+// Fonctions pour le déplacement du sous-marin
+
+void avance(int vitesse, int t){
+    // Fait avancer le moteur (vitesse absolue)
+    moteur1.writeMicroseconds(vitesse);
+    moteur2.writeMicroseconds(vitesse);
+    delay(t);
+    moteur1.writeMicroseconds(throttleNeutre);
+    moteur2.writeMicroseconds(throttleNeutre);
+}
+
+void tourne_gauche(int delta_v, int t){
+    // Fait tourner à gauche (vitesse relative par rapport au point mort v=1500)
+    moteur1.writeMicroseconds(1500+delta_v);
+    moteur2.writeMicroseconds(1500-delta_v);
+    delay(t);
+    moteur1.writeMicroseconds(throttleNeutre);
+    moteur2.writeMicroseconds(throttleNeutre);
+}
+
+void tourne_droite(int delta_v, int t){
+    // Fait tourner à gauche (vitesse relative par rapport au point mort v=1500)
+    moteur1.writeMicroseconds(1500-delta_v);
+    moteur2.writeMicroseconds(1500+delta_v);
+    delay(t);
+    moteur1.writeMicroseconds(throttleNeutre);
+    moteur2.writeMicroseconds(throttleNeutre);
+}
+
+void arret(){
+    // Arrête les moteurs en cas de problème
+    moteur1.writeMicroseconds(throttleNeutre);
+    moteur2.writeMicroseconds(throttleNeutre);
+}
+
+void calibrerESCs() {
+    // Calibrage des moteurs
+    Serial.println("Calibration des ESCs...");
+    Serial.println("Débranchez les batteries, puis branchez-les quand vous y êtes invité.");
+    delay(3000);
+    Serial.println("Branchez les batteries MAINTENANT !");
+    
+    // Plein gaz pour init calibration
+    avance(throttleAvant,3000);
+  
+    // Retour au minimum pour terminer la calibration
+    arret();
+    Serial.println("Calibration terminée !");
+    delay(2000);
+  }
